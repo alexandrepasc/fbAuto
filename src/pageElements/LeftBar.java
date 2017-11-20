41cf5b516f65315f589a01d2c88c5b9f60d8b2e9
alexandrepasc <alexandre.pascoal@gmail.com>
2017-11-20 16:42:41 +0000

    add elements to LeftBar class

---
+++ src/pageElements/LeftBar.java
@@ -0,0 +1,60 @@
+package pageElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import common.Logger_;
+
+public class LeftBar {
+
+	public static WebElement Bar(WebDriver driver_) {
+		try {
+			return driver_.findElement(By.id("sideNav"));
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e, driver_);
+			return null;
+		}
+	}
+	
+	public static WebElement ProfileBut(WebDriver driver_) {
+		try {
+			return driver_.findElement(By.xpath("//div[contains(@data-testid, 'left_nav_item_TheGuren Guren')]")); //NEED TO BE CHANGED FOR OTHER ACCOUNTS
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e, driver_);
+			return null;
+		}
+	}
+	
+	public static WebElement Shortcuts(WebDriver driver_) {
+		try {
+			return driver_.findElement(By.id("pinnedNav"));
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e, driver_);
+			return null;
+		}
+	}
+	
+	public static WebElement Explore(WebDriver driver_) {
+		try {
+			return driver_.findElement(By.id("appsNav"));
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e, driver_);
+			return null;
+		}
+	}
+	
+	public static WebElement Create(WebDriver driver_) {
+		try {
+			return driver_.findElement(By.id("createNav"));
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e, driver_);
+			return null;
+		}
+	}
+}
