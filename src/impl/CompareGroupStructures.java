749d6a463236d87331f76cac3ccc812744557f56
alexandrepasc <alexandre.pascoal@gmail.com>
2017-12-04 18:35:58 +0000

    add methods to CompareGroupStructures class

--- src/impl/CompareGroupStructures.java
+++ src/impl/CompareGroupStructures.java
@@ -8,6 +8,42 @@ public class CompareGroupStructures {
 	public static boolean Compare(GroupStructure[] fromFileStructure_, GroupStructure[] fromWebStructure_) {
 		try {
 			
+			if (fromFileStructure_.length == fromWebStructure_.length) {
+				
+				Logger_.Logging_(Thread.currentThread().getStackTrace()[1] + " - NO Group Changes", "info");
+			}
+			else {
+				
+			}
+			
+			return true;
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e);
+			return false;
+		}
+	}
+	
+	private static boolean CehckDifferences(GroupStructure[] fromFileStructure_, GroupStructure[] fromWebStructure_) {
+		try {
+			if (fromFileStructure_.length > fromWebStructure_.length) {
+				
+			}
+			else if (fromFileStructure_.length < fromWebStructure_.length) {
+				
+			}
+			
+			return true;
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e);
+			return false;
+		}
+	}
+	
+	private static boolean DetectDifferences(GroupStructure[] fromFileStructure_, GroupStructure[] fromWebStructure_) {
+		try {
+			
 			return true;
 		}
 		catch (Exception e) {
