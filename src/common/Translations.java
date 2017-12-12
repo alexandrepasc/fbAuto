fb9da2ec28f47934a53ae9d80889053ffa87d797
alexandrepasc <alexandre.pascoal@gmail.com>
2017-12-12 14:55:10 +0000

    add new translations to some drop down menu items

--- src/common/Translations.java
+++ src/common/Translations.java
@@ -24,6 +24,78 @@ public class Translations {
 		}
 	}
 	
+	public static String DropMenuCreatePage(WebDriver driver_) {
+		try {			
+			switch (GetLanguage(driver_)) {
+				case ENG:
+					return "Create Page";
+				case PT:
+					return "Criar Página";
+				default:
+					Logger_.Logging_(Thread.currentThread().getStackTrace()[1] + " - Problem Getting Language", "info");
+					return null;
+			}
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage() + " - Problem Getting Language", "severe", e);
+			return null;
+		}
+	}
+	
+	public static String DropMenuManagePages(WebDriver driver_) {
+		try {
+			switch (GetLanguage(driver_)) {
+				case ENG:
+					return "Manage Pages";
+				case PT:
+					return "Gerir Páginas";
+				default:
+					Logger_.Logging_(Thread.currentThread().getStackTrace()[1] + " - Problem Getting Language", "info");
+					return null;
+			}
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage() + " - Problem Getting Language", "severe", e);
+			return null;
+		}
+	}
+	
+	public static String DropMenuManagePage(WebDriver driver_) {
+		try {
+			switch (GetLanguage(driver_)) {
+				case ENG:
+					return "Manage Page";
+				case PT:
+					return "Gerir Página";
+				default:
+					Logger_.Logging_(Thread.currentThread().getStackTrace()[1] + " - Problem Getting Language", "info");
+					return null;
+			}
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage() + " - Problem Getting Language", "severe", e);
+			return null;
+		}
+	}
+	
+	public static String DropMenuLogout(WebDriver driver_) {
+		try {
+			switch (GetLanguage(driver_)) {
+				case ENG:
+					return "Log Out";
+				case PT:
+					return "Terminar Sessão";
+				default:
+					Logger_.Logging_(Thread.currentThread().getStackTrace()[1] + " - Problem Getting Language", "info");
+					return null;
+			}
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage() + " - Problem Getting Language", "severe", e);
+			return null;
+		}
+	}
+	
 	private static Languages GetLanguage(WebDriver driver_) {
 		try {
 			final String aux_ = TopBar.ButHomePage(driver_).getText();//.substring(0, TopBar.ButHomePage(driver_).getText().length() - 1);
