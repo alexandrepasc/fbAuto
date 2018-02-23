ea49e5a66ffa8de9eb6e2735e3cae78e0a018243
apascoal <apascoal@bikeemotion.com>
2018-02-23 12:12:52 +0000

    Add to post structures #33

---
+++ src/common/structures/ToPost.java
@@ -0,0 +1,12 @@
+package common.structures;
+
+public class ToPost {
+
+	public String done;
+	
+	public String postText;
+	
+	public String postUrl;
+	
+	public ToPostGroup[] groups;
+}
---
+++ src/common/structures/ToPostGroup.java
@@ -0,0 +1,5 @@
+package common.structures;
+
+public class ToPostGroup {
+
+}
