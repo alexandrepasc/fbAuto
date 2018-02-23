0ba7f29a2850a774bc8adc18e25e1184eaf6b827
apascoal <apascoal@bikeemotion.com>
2018-02-23 16:08:29 +0000

    Add another read file class, not tested. Check issue #17

---
+++ src/impl/group/ReadFile.java
@@ -0,0 +1,96 @@
+package impl.group;
+
+import java.io.File;
+import java.lang.reflect.Field;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import common.Logger_;
+import common.structures.ToPost;
+
+public class ReadFile {
+
+	public static boolean Reading(File file_) {
+		try {
+			
+			Document doc_;
+			
+			if ((doc_ = OpenXML(file_)) == null) {
+				return false;
+			}
+			
+			ToPost toPostStructure_ = new ToPost();
+			
+			if ((toPostStructure_ = ReadToStructure(doc_, toPostStructure_)) == null) {
+				return false;
+			}
+						
+			return true;
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e);
+			return false;
+		}
+	}
+	
+	private static Document OpenXML(File file_) {
+		try {
+			
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc_ = dBuilder.parse(file_);
+			doc_.getDocumentElement().normalize();
+			
+			return doc_;
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e);
+			return null;
+		}
+	}
+	
+	private static ToPost ReadToStructure(Document doc_, ToPost toPostStructure_) {
+		try {
+			
+			if (doc_.hasChildNodes()) {
+				
+				Node tempNode_ = doc_.getChildNodes().item(0);
+					
+				NodeList nList_ = tempNode_.getChildNodes();
+				
+				if ((toPostStructure_ = GetPostValues(nList_, toPostStructure_)) == null) {
+					return null;
+				}
+			}
+			
+			return toPostStructure_;
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e);
+			return null;
+		}
+	}
+	
+	private static ToPost GetPostValues(NodeList nList_, ToPost toPostStructure_) {
+		try {
+			
+			for (int i = 0; i < (nList_.getLength() - 1); i++) {
+				
+				Field structField_ = toPostStructure_.getClass().getDeclaredField(nList_.item(i).getNodeName());
+				
+				structField_.set(toPostStructure_, nList_.item(i).getTextContent());
+			}
+			
+			return toPostStructure_;
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e);
+			return null;
+		}
+	}
+}
