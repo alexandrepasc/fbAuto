3ab76e28971fda979b65a4ffb1d1608d4860f3fc
apascoal <apascoal@bikeemotion.com>
2018-03-02 15:52:30 +0000

    Add flow.

--- src/impl/group/PublishPost.java
+++ src/impl/group/PublishPost.java
@@ -1,6 +1,5 @@
 package impl.group;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import common.Logger_;
@@ -18,6 +17,9 @@ public class PublishPost {
 			if (!PasteText(driver_, text_, link_)) {
 				return false;
 			}
+			
+			Group.PublishPostBut(driver_).click();
+			Logger_.Logging_(Thread.currentThread().getStackTrace()[1] + " - Publish Post Buttton Click", "info");
 			
 			//FOR TESTING
 			Thread.sleep(15000);
@@ -47,14 +49,38 @@ public class PublishPost {
 	private static boolean PasteText(WebDriver driver_, String text_, String link_) {
 		try {
 			
-			Group.PostOpenTextFormInput(driver_).sendKeys(text_ + link_);
+			String[] aux_ = SplitText(text_);
+			for (int i = 0; i < aux_.length; i++) {
+				
+				Group.PostOpenTextFormInput(driver_).sendKeys(aux_[i] + "\n");
+				//System.out.println(aux_[i]);
+			}
+			
+			Group.PostOpenTextFormInput(driver_).sendKeys(link_);
+						
 			Logger_.Logging_(Thread.currentThread().getStackTrace()[1] + " - Post Text Open Form Set Text", "info");
+			
+			//JUST FOR TESTS
+			Thread.sleep(50000);
 			
 			return true;
 		}
 		catch (Exception e) {
 			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e, driver_);
 			return false;
+		}
+	}
+	
+	private static String[] SplitText(String text_) {
+		try {
+			
+			String[] aux_ = text_.split("/n");
+			
+			return aux_;
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e);
+			return null;
 		}
 	}
 }
