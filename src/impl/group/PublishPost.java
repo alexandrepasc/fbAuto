73b0ed89c503f7416577b8fa8f49d5ec43b5d4f7
apascoal <apascoal@bikeemotion.com>
2018-03-05 11:14:34 +0000

    Changes on the flow.

--- src/impl/group/PublishPost.java
+++ src/impl/group/PublishPost.java
@@ -8,6 +8,7 @@ import java.awt.event.KeyEvent;
 
 import org.openqa.selenium.WebDriver;
 
+import common.Comm;
 import common.Logger_;
 import pageElements.Group;
 
@@ -16,9 +17,9 @@ public class PublishPost {
 	public static boolean Pub(WebDriver driver_, String text_, String link_) {
 		try {
 			
-			/*if (!OpenText(driver_)) {
+			if (!OpenText(driver_)) {
 				return false;
-			}*/
+			}
 			
 			if (!PasteText(driver_, text_, link_)) {
 				return false;
@@ -43,6 +44,31 @@ public class PublishPost {
 			
 			Group.PostTextForm(driver_).click();
 			Logger_.Logging_(Thread.currentThread().getStackTrace()[1] + " - Post Text Form Click", "info");
+			
+			if (!CheckPublishElements(driver_)) {
+				return false;
+			}
+			
+			return true;
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e, driver_);
+			return false;
+		}
+	}
+	
+	private static boolean CheckPublishElements(WebDriver driver_) {
+		try {
+			
+			if (!Comm.checkElement(Group.PostOpenTextForm(driver_), driver_)) {
+				Logger_.Logging_(Thread.currentThread().getStackTrace()[1] + " - Post Text Open Form IS NOT Present and/or Visible", "info");
+				return false;
+			}
+			
+			if (!Comm.checkElement(Group.PublishPostBut(driver_), driver_)) {
+				Logger_.Logging_(Thread.currentThread().getStackTrace()[1] + " - Publish Post Open Button IS NOT Present and/or Visible", "info");
+				return false;
+			}
 			
 			return true;
 		}
@@ -78,7 +104,7 @@ public class PublishPost {
 			Logger_.Logging_(Thread.currentThread().getStackTrace()[1] + " - Post Text Open Form Set Text", "info");
 			
 			//JUST FOR TESTS
-			Thread.sleep(50000);
+			//Thread.sleep(50000);
 			
 			return true;
 		}
