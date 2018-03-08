321bf5f6953201af3f169b93655e46e5be0dde04
apascoal <apascoal@bikeemotion.com>
2018-03-08 13:08:11 +0000

    Add flow for toPost write file. ref #52

--- src/common/StructureToArray.java
+++ src/common/StructureToArray.java
@@ -3,6 +3,7 @@ package common;
 import common.structures.GroupStructure;
 import common.structures.PagePostsStructure;
 import common.structures.PageStructure;
+import common.structures.ToPost;
 
 public class StructureToArray {
 
@@ -51,6 +52,35 @@ public class StructureToArray {
 				//array_[i][0] = pageStructure_[i].name;
 				array_[i][0] = pageStructure_[i].url;
 				array_[i][1] = pageStructure_[i].text;
+			}
+			
+			return array_;
+		}
+		catch (Exception e) {
+			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e);
+			return null;
+		}
+	}
+
+	public static String[][] ConvertToArray(ToPost toPostStructure_) { //COULD BE CHANGES TO NOT BE HARD CODED
+		try {
+			String[][] array_ = new String[1][4 + (toPostStructure_.groups.length * 2)];
+			
+			array_[0][0] = toPostStructure_.done;
+			array_[0][1] = toPostStructure_.postText;
+			array_[0][2] = toPostStructure_.postUrl;
+			array_[0][3] = String.valueOf(toPostStructure_.groups.length);
+			
+			int aux_ = 0;
+			for (int i = 4; i < array_[0].length; i++) {
+				
+				array_[0][i] = toPostStructure_.groups[aux_].name;
+				
+				i += 1;
+				
+				array_[0][i] = toPostStructure_.groups[aux_].url;
+				
+				aux_ += 1;
 			}
 			
 			return array_;
--- src/common/StructureType.java
+++ src/common/StructureType.java
@@ -3,5 +3,6 @@ package common;
 public enum StructureType {
 	GROUP,
 	PAGE,
-	POSTS
+	POSTS,
+	TOPOST
 }
