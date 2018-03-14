fb749f18c86296f8bf1b3c72a5b192400e0c3e54
apascoal <apascoal@bikeemotion.com>
2018-03-14 15:57:19 +0000

    Add the cmd to change the permissions and move file. ref #60

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -4,3 +4,7 @@ value=$(<../config)
 echo "$value"
 
 docker run -it -d --name fbauto -v $value:/home/seluser/fbAuto/app fbauto /bin/bash
+
+docker exec -it fbauto bash -c "bash permissions.sh"
+
+docker exec -it fbauto bash -c "mv config.xml app/config.xml"
