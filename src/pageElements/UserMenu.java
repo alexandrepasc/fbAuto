43572ee33c48edc78d2345e68e7a37fef05da037
apascoal <apascoal@bikeemotion.com>
2018-03-15 18:15:40 +0000

    Clean pageElements package classes. ref #53

--- src/pageElements/UserMenu.java
+++ src/pageElements/UserMenu.java
@@ -13,7 +13,6 @@ public class UserMenu {
 	public static WebElement ButDropMenu(WebDriver driver_) {
 		try {
 			return driver_.findElement(By.id("logoutMenu"));
-			//return driver_.findElement(By.id("userNavigationLabel"));
 		}
 		catch (Exception e) {
 			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e, driver_);
@@ -37,8 +36,6 @@ public class UserMenu {
 	
 	public static WebElement[] ListButDropMenu(WebDriver driver_) {
 		try {
-			/*List<WebElement> list_ = driver_.findElement(By.xpath("/html/body/div[23]/div/div/div/div/div[1]/div/div/ul"))
-					.findElements(By.tagName("li"));*/
 			List<WebElement> list_ = driver_.findElement(By.xpath("//ul[contains(@role, 'menu')]"))
 					.findElements(By.tagName("li"));
 			
@@ -53,9 +50,6 @@ public class UserMenu {
 	public static WebElement ButLogout(WebDriver driver_) {
 		try {
 			return ListButDropMenu(driver_)[ListButDropMenu(driver_).length - 1];
-			
-			//return driver_.findElement(By.xpath("/html/body/div[22]/div/div/div/div/div[1]/div/div/ul/li[19]/a"));
-			//return driver_.findElement(By.xpath("//*[@id='js_9o']/div/div/ul/li[18]/a/span/span"));
 		}
 		catch (Exception e) {
 			Logger_.Logging_(e.getMessage() + e.getLocalizedMessage(), "severe", e, driver_);
